f4f036cb0c036258e4f8ba6aebaebf8054d9bbcb
Vitor Satake <vitor.scastro2013@hotmail.com>
2023-10-27 16:10:20 -0300

    atualizando sistema de adocao

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',function(){return view('welcome');});
+Route::get('/painel',function(){return view('painel.painel');});
+Route::get('/editar',function(){return view('painel.editar');});
+Route::get('/home',function(){return view('site.index');});
+Route::get('/quero-adotar',function(){return view('site.quero-adotar');});
+Route::get('/integra',function(){return view('site.integra');});
+Route::get('/login',function(){return view('painel.login');});
+Route::get('/painel',function(){return view('painel.painel');});
+Route::get('/formulario',function(){return view('site.formulario');});
+Route::get('/recuperar-senha',function(){return view('painel.recuperar-senha');});
+Route::get('/cadastrar',function(){return view('painel.cadastrar');});
